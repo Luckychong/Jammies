7fdc10ca7c77c266b89d4f6f20da90c014a1b45d
Luckychong <lucky0212@yahoo.com>
2020-04-21 01:29:30 +0000

    almost there

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 
 $("#search-button").click(function(){
     var things = $("input").val()
+
     
-    fetch("https://api.soundcloud.com/tracks?q=meg&client_id=5aa8e389ba4e24b6106af5159ab3e344")
+    fetch("https://api.soundcloud.com/tracks?q="+things+ "&client_id=5aa8e389ba4e24b6106af5159ab3e344")
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         // var pic_url = data.data[0].images.original.url
-        $(".container").html("<h1>" + data + "</h1>")
+    
+        data.forEach(function(song){
+	     $(".container").html("<h1>" + data[0].title + "</h1>")
 
-        //$(".container").append(`<img src=` + pic_url + '>');
-
+        $(".container").append(`<img src=` + data[0].artwork_url + '>');
+	    
+	    });
     }); 
 
-
-  
-  
 });
